29be73a7c11478130bd625122388f56ba9802d16
Marcos Martins <marcos_36ma@hotmail.com>
2021-03-08 21:22:25 -0300

    Primeiro codigo

---
+++ src/main/java/com/powerdevs/vetsapi/VetsapiApplication.java
@@ -0,0 +1,13 @@
+package com.powerdevs.vetsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/powerdevs/vetsapi/VetsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerdevs.vetsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
