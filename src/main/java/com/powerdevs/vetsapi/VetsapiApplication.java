ddf612df19f31f6bf6436b1a9d689b5393ac75ae
Marcos Martins <marcos_36ma@hotmail.com>
2021-05-03 19:45:24 -0300

    Adicionado mensagens na rota inicial e no console

--- src/main/java/com/powerdevs/vetsapi/VetsapiApplication.java
+++ src/main/java/com/powerdevs/vetsapi/VetsapiApplication.java
@@ -9,5 +9,14 @@ public class VetsapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VetsapiApplication.class, args);
 		System.out.println("-------------------------------- O sistema foi carregado --------------------------------");
+		System.out.println(
+			"		#     # ####### #######  #####           #    ######  ### \n" +
+			"		#     # #          #    #     #         # #   #     #  #  \n" +
+			"		#     # #          #    #              #   #  #     #  #  \n" +
+			"		#     # #####      #     #####  ##### #     # ######   #  \n" +
+			" 		 #   #  #          #          #       ####### #        #  \n" +
+			" 		  # #   #          #    #     #       #     # #        #  \n" +
+			"  		   #    #######    #     #####        #     # #       ### \n" +
+			"                                                          \n");
 	}
 }
---
+++ src/main/java/com/powerdevs/vetsapi/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.powerdevs.vetsapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home(){
+        return  "<p>Vets-Api - Api Rest que que localiza a clínica veterinária mais próxima</p>\n"+
+                "<p>Rotas:<p>\n"+
+                "<p>GET /location</p>\n"+
+                "<p>GET /details</p>\n";
+    }
+
+}
